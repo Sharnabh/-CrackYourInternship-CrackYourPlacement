0c0150d4822443f6b98381d54d376cc91dbe796f
Sharnabh Banerjee <banerjeesharnabh@gmail.com>
2024-07-25 13:12:31 +0530

    Day 10

---
+++ Strings/Print Anagrams Together.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+from collections import defaultdict
+
+class Solution:
+    def Anagrams(self, words, n):
+        '''
+        words: list of word
+        n:      no of words
+        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
+        '''
+        
+        #code here
+        anagram_map = defaultdict(list)
+
+        for i in words:
+            sorted_word = ''.join(sorted(i))
+            anagram_map[sorted_word].append(i)
+
+        return list(anagram_map.values())
+
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ =='__main__':
+    t= int(input())
+    for tcs in range(t):
+        n= int(input())
+        words=input().split()
+        
+        ob = Solution()
+        ans = ob.Anagrams(words,n)
+        
+        for grp in sorted(ans):
+            for word in grp:
+                print(word,end=' ')
+            print()
+
+# } Driver Code Ends
